d1f827e9267691cec7ca2dcb26484b0d15b5fc3a
ThiagoJoseSousa <imigrationstudy@gmail.com>
2022-09-02 13:39:40 -0300

    All components created and properly added.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-
+// return () is for html, return {} for js
 import React,{Component} from 'react';
 import IdentityDisplay, {IdentityEdit} from './components/Identity';
 import EducationalXpDisplay, {EducationalXpEdit} from './components/Educational xp'
-
+import PracticalXpDisplay, {PracticalXpEdit} from './components/Practical xp'
 class App extends Component {
 constructor(){
   super()
   this.state= {
     isIdentityEdit:true,
     isSchoolXpEdit:true,
+    isPracticalXpEdit:true
   }
   this.handleIdentitySubmit=this.handleIdentitySubmit.bind(this)
   this.toggleEdit=this.toggleEdit.bind(this)
   this.handleEducationalXpSubmit=this.handleEducationalXpSubmit.bind(this)
+  this.handlePracticalXpSubmit=this.handlePracticalXpSubmit.bind(this) // bind is really important
 }
 handleIdentitySubmit (info){//info = state passed as argument
 this.setState((prevState)=> {
@@ -37,6 +39,20 @@ handleEducationalXpSubmit(info) {
   }})
 }
 
+handlePracticalXpSubmit(info) {
+ this.setState((prevState)=> {
+  return {
+    info: {company:info.company,
+      position:info.position,
+      tasks:info.tasks,
+      dateTimeWorked:info.dateTimeWorked
+
+    },
+    isPracticalXpEdit:!prevState.isPracticalXpEdit
+  }
+ }) 
+}
+
 toggleEdit (valueName) {
 this.setState ((prevState)=> {
 let newState= {}
@@ -46,7 +62,7 @@ return newState
 }
 
   render(){ //when does render happen? Every update to the values
-  return (
+  return ( 
     <div className='App'>
       {this.state.isIdentityEdit? //this acess returns true or false, this item was created on constructor (created first)
       <IdentityDisplay submit={this.handleIdentitySubmit} info={this.state.info}/>: /* state is changed on submit bttn/enter bttn, but on submit It also alters the isEditable state, which is used to define what to render
@@ -58,6 +74,10 @@ return newState
          this.state.isSchoolXpEdit? 
       <EducationalXpDisplay submit={this.handleEducationalXpSubmit} info={this.state.info}/> : 
       <EducationalXpEdit submit={()=> this.toggleEdit('isSchoolXpEdit')} info={this.state.info}/>
+      }
+      {this.state.isPracticalXpEdit? 
+        <PracticalXpDisplay submit={this.handlePracticalXpSubmit} info={this.state.info}/> :
+        <PracticalXpEdit submit={()=> this.toggleEdit('isPracticalXpEdit')} info={this.state.info}/>
       }
     </div>
   )
--- src/components/Practical xp.js
+++ src/components/Practical xp.js
@@ -2,9 +2,71 @@
 // constructor is jsut a keyword to the first executed method of the class 
 //super calls the constructor of the  parent
 
-import React from "react";
+import React, {Component} from "react";
 
-class PracticalXp extends React.Component {
+class PracticalXpDisplay extends Component {
+constructor (props) {
+    super()
+    this.state={company:props.info? props.info.company:'', 
+    position:props.info? props.info.position:'',
+    tasks:props.info? props.info.tasks:'',
+    dateTimeWorked:props.info? props.info.dateTimeWorked:''
+}
+this.handleInputChange=this.handleInputChange.bind(this)
+this.handleSubmit=this.handleSubmit.bind(this)
+}
 
+handleInputChange(e,valueName){
+this.setState(prevState => {
+    let newState= {...prevState}
+    newState[valueName]=e.target.value
+    return newState;
+})
+}
+handleSubmit=(e)=> {
+e.preventDefault()
+this.props.submit(this.state)
+}
+
+
+render() {
+    return (<div><form>
+        <h2>Practical experiecne</h2>
+        <label htmlFor="company"> Company name:
+<input type='text' name='company' value={this.state.company} onChange={(e)=> {
+    this.handleInputChange(e, 'company')
+}}/></label><br />
+<label htmlFor="position"> Position:
+<input type='text' name='position' value={this.state.position} onChange={(e)=> {
+    this.handleInputChange(e, 'position')
+}}/></label><br />
+
+<label htmlFor="tasks"> Main task(s):
+<input type='text' name='tasks' value={this.state.tasks} onChange={(e)=> {
+    this.handleInputChange(e, 'tasks')
+}}/></label><br />
+
+<label htmlFor="dateTimeWorked"> Beggining/end date of working: 
+<input type='text' name='dateTimeWorked' value={this.state.dateTimeWorked} onChange={(e)=> {
+    this.handleInputChange(e, 'dateTimeWorked')
+}}/></label><br />
+<button onClick={this.handleSubmit}> Submit</button>
+
+</form>
+    </div>)
+}
     
 }
+
+export class PracticalXpEdit extends Component{
+render (){ return(
+<div><form> <h2> Practical Experience</h2>
+<p> Company name:{this.props.info.company}</p>
+<p> Position:{this.props.info.position}</p>
+<p> Main task:{this.props.info.tasks}</p>
+<p> Start and end date of work:{this.props.info.dateTimeWorked}</p>
+</form>
+</div>)
+}
+}
+export default PracticalXpDisplay
